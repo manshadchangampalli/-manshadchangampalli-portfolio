34880610310a56ffd5b589fc78ca26ff40f50e43
manshad <manshadchangampalli010@gmail.com>
2021-05-06 23:53:40 +0530

    the navbar and home section is almost finishd

--- app.js
+++ app.js
@@ -1,27 +1,9 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-dark ">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="portfolio collapse navbar-collapse " id="navbarTogglerDemo01">
-          <a id="portfolio" class="text-white navbar-brand ml-4 " href="#">Portfo<span>lio.</span></a>
-          <ul  class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item   active mr-3">
-              <a class="skills nav-link " href="#"><span>Home</span> <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item  mr-3">
-              <a class="skills nav-link " href="#"><span>About</span></a>
-            </li>
-            <li class="nav-item mr-3">
-                <a class="skills nav-link " href="#"><span>Skills</span></a>
-              </li>
-              <li class="nav-item mr-3">
-                <a class="skills nav-link " href="#"><span>Projects</span></a>
-              </li>
-              <li class="nav-item mr-3">
-                <a class="skills nav-link " href="#"><span>Projects</span></a>
-              </li>
-           
-          </ul>
-          
-        </div>
-      </nav>
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY>150){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    })
+})
