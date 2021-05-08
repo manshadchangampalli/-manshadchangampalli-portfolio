d7a919e9ab6573d499c3242dda7d0ea0ccdd394e
manshad <manshadchangampalli010@gmail.com>
2021-05-08 14:46:37 +0530

    header section put the scrolling effect, and text put the auto typing effect, and the home section and header section completely finished, and the about section layout almost finished

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+
+
 $(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY>150){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-    })
+$(window).scroll(function(){
+    if(this.scrollY>150){
+        $('.navbar').addClass("sticky");
+    }else{
+        $('.navbar').removeClass("sticky");
+    }
 })
+})
+$(document).ready(function(){
+$(window).scroll(function(){
+    if(this.scrollY>30){
+        $('.navbar').addClass("mobile");
+    }else{
+        $('.navbar').removeClass("mobile");
+    }
+})
+})
+   
